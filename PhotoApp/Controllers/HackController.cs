7067047ecbe68f9cb74dbc3e1272f82f4733a534
digi <digi@skbkontur.ru>
2019-04-24 15:20:35 +0500

    token task

---
+++ PhotoApp/Controllers/HackController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PhotoApp.Services;
+
+namespace PhotoApp.Controllers
+{
+    public class HackController : Controller
+    {
+        [HttpGet("hack/super_secret_qwe123")]
+        public IActionResult GenerateToken()
+        {
+            var encodedJwt = TemporaryTokens.GenerateEncoded();
+
+            Response.Cookies.Append(TemporaryTokens.CookieName, encodedJwt,
+                new CookieOptions {HttpOnly = true});
+            return Content(encodedJwt);
+        }
+    }
+}
---
+++ PhotoApp/Services/TemporaryTokens.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace PhotoApp.Services
+{
+    public static class TemporaryTokens
+    {
+        public static SymmetricSecurityKey SigningKey =>
+            new SymmetricSecurityKey(Encoding.ASCII.GetBytes("Ne!0_0!vzlomayesh!^_^!nicogda!"));
+
+        public const string CookieName = "TemporaryToken";
+
+        public static string GenerateEncoded()
+        {
+            var claims = new Claim[]
+            {
+            };
+
+            var jwt = new JwtSecurityToken(
+                claims: claims,
+                notBefore: null,
+                expires: null,
+                signingCredentials: null);
+            var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+            return encodedJwt;
+        }
+    }
+}
