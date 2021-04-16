fcf762eecd4e11156646fbb4b5966322549d8482
Домашних Иван <digi@skbkontur.ru>
2021-04-16 18:27:27 +0500

    Solved 1.5.

--- PhotosApp/Areas/Identity/IdentityHostingStartup.cs
+++ PhotosApp/Areas/Identity/IdentityHostingStartup.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using PhotosApp.Areas.Identity.Data;
+using PhotosApp.Services;
 
 [assembly: HostingStartup(typeof(PhotosApp.Areas.Identity.IdentityHostingStartup))]
 namespace PhotosApp.Areas.Identity
@@ -19,8 +20,26 @@ namespace PhotosApp.Areas.Identity
                     options.UseSqlite(
                         context.Configuration.GetConnectionString("UsersDbContextConnection")));
 
-                services.AddDefaultIdentity<PhotosAppUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                    .AddEntityFrameworkStores<UsersDbContext>();
+                services.AddDefaultIdentity<PhotosAppUser>()
+                    .AddEntityFrameworkStores<UsersDbContext>()
+                    .AddPasswordValidator<UsernameAsPasswordValidator<PhotosAppUser>>();
+
+                services.Configure<IdentityOptions>(options =>
+                {
+                    options.Password.RequireDigit = false;
+                    options.Password.RequireLowercase = true;
+                    options.Password.RequireNonAlphanumeric = false;
+                    options.Password.RequireUppercase = false;
+                    options.Password.RequiredLength = 6;
+                    options.Password.RequiredUniqueChars = 1;
+                });
+
+                services.Configure<IdentityOptions>(options =>
+                {
+                    options.SignIn.RequireConfirmedAccount = false;
+                    options.SignIn.RequireConfirmedEmail = false;
+                    options.SignIn.RequireConfirmedPhoneNumber = false;
+                });
             });
         }
     }
