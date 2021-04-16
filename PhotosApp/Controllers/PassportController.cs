eb6358ad10acbdc782c97392796d4a1b79b56091
Домашних Иван <digi@skbkontur.ru>
2021-04-16 18:28:37 +0500

    Solved 9.2.

---
+++ PhotosApp/Controllers/PassportController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PhotosApp.Controllers
+{
+    public class PassportController : Controller
+    {
+        // NOTE: Неаутентифицированный пользователь будет отправляться на вход в DefaultChallengeScheme,
+        // а затем возвращаться сюда и отсюда перенаправляться на исходную страницу из returnUrl.
+        public IActionResult Login(string returnUrl)
+        {
+            if (!User.Identity.IsAuthenticated)
+                return Challenge();
+            
+            return Redirect(Url.IsLocalUrl(returnUrl) ? returnUrl : "/");
+        }
+
+        // NOTE: Выход из текущей схемы аутентификации с последующей переадресацией
+        [Authorize]
+        public IActionResult Logout()
+        {
+            return SignOut(new AuthenticationProperties
+            {
+                RedirectUri = "/"
+            });
+        }
+    }
+}
--- PhotosApp/Startup.cs
+++ PhotosApp/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ using PhotosApp.Clients;
 using PhotosApp.Clients.Models;
 using PhotosApp.Data;
 using PhotosApp.Models;
+using PhotosApp.Services.Authorization;
 using Serilog;
 
 namespace PhotosApp
@@ -61,6 +63,85 @@ namespace PhotosApp
             }, new System.Reflection.Assembly[0]);
 
             services.AddTransient<ICookieManager, ChunkingCookieManager>();
+
+            services.AddAuthentication()
+                .AddOpenIdConnect("Passport", "Паспорт", options =>
+                {
+                    options.Authority = "https://localhost:7001";
+
+                    options.ClientId = "Photos App by OIDC";
+                    options.ClientSecret = "secret";
+                    options.ResponseType = "code";
+
+                    // NOTE: oidc и profile уже добавлены по умолчанию
+                    options.Scope.Add("email");
+
+                    options.CallbackPath = "/signin-passport";
+
+                    // NOTE: все эти проверки токена выполняются по умолчанию, указаны для ознакомления
+                    options.TokenValidationParameters.ValidateIssuer = true; // проверка издателя
+                    options.TokenValidationParameters.ValidateAudience = true; // проверка получателя
+                    options.TokenValidationParameters.ValidateLifetime = true; // проверка не протух ли
+                    options.TokenValidationParameters.RequireSignedTokens = true; // есть ли валидная подпись издателя
+                });
+
+            services.AddScoped<IAuthorizationHandler, MustOwnPhotoHandler>();
+            services.AddAuthorization(options =>
+            {
+                options.DefaultPolicy = new AuthorizationPolicyBuilder()
+                    .RequireAuthenticatedUser()
+                    .Build();
+                options.AddPolicy(
+                    "Beta",
+                    policyBuilder =>
+                    {
+                        policyBuilder.RequireAuthenticatedUser();
+                        policyBuilder.RequireClaim("testing", "beta");
+                    });
+                options.AddPolicy(
+                    "CanAddPhoto",
+                    policyBuilder =>
+                    {
+                        policyBuilder.RequireAuthenticatedUser();
+                        policyBuilder.RequireClaim("subscription", "paid");
+                    });
+                options.AddPolicy(
+                    "MustOwnPhoto",
+                    policyBuilder =>
+                    {
+                        policyBuilder.RequireAuthenticatedUser();
+                        policyBuilder.AddRequirements(new MustOwnPhotoRequirement());
+                    });
+                options.AddPolicy(
+                    "Dev",
+                    policyBuilder =>
+                    {
+                        policyBuilder.RequireAuthenticatedUser();
+                        // policyBuilder.RequireRole("Dev");
+                        // policyBuilder.AddAuthenticationSchemes(
+                        //     JwtBearerDefaults.AuthenticationScheme,
+                        //     IdentityConstants.ApplicationScheme);
+                    });
+            });
+
+            services.AddAuthentication(options =>
+            {
+                // NOTE: Схема, которую внешние провайдеры будут использовать для сохранения данных о пользователе
+                // NOTE: Так как значение совпадает с DefaultScheme, то эту настройку можно не задавать
+                options.DefaultSignInScheme = "Cookie";
+                // NOTE: Схема, которая будет вызываться, если у пользователя нет доступа
+                options.DefaultChallengeScheme = "Passport";
+                // NOTE: Схема на все остальные случаи жизни
+                options.DefaultScheme = "Cookie";
+            })
+            .AddCookie("Cookie", options =>
+            {
+                // NOTE: Пусть у куки будет имя, которое расшифровывается на странице «Decode»
+                options.Cookie.Name = "PhotosApp.Auth";
+                // NOTE: Если не задать здесь путь до обработчика logout, то в этом обработчике
+                // будет игнорироваться редирект по настройке AuthenticationProperties.RedirectUri
+                options.LogoutPath = "/Passport/Logout";
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -86,7 +167,6 @@ namespace PhotosApp
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute("default", "{controller=Photos}/{action=Index}/{id?}");
-                endpoints.MapRazorPages();
             });
         }
     }
