a581db9c1ecbc6c1f1134ddfbd372d35ea3715b7
Домашних Иван <digi@skbkontur.ru>
2021-04-27 16:39:53 +0500

    Fix Photos.js

--- PhotosSPA/ClientApp/src/components/photos/Photos.js
+++ PhotosSPA/ClientApp/src/components/photos/Photos.js
@@ -10,31 +10,45 @@ export class Photos extends Component {
     super(props);
     // NOTE: Сразу после перехода пользователя на страницу фотографий состояние такое:
     // фотографий еще нет, но загрузка уже ведется.
-    this.state = { photos: [], loading: true };
+    this.state = { photos: [], loading: true, errorMessage: null };
   }
 
   componentDidMount() {
     this.populatePhotos();
   }
 
-  // NOTE: Логика отрисовки страницы такая:
-  // в зависимости от состояния либо показывается надпись «Загрузка...»,
-  // либо показывается информация о фотографиях.
+  // NOTE: Здесь формируется разметка страницы
   render() {
-    const contents = this.state.loading ? (
-      <p>
-        <em>Загрузка...</em>
-      </p>
-    ) : (
-      this.renderPhotos(this.state.photos)
-    );
-
     return (
       <div className="photosContainer">
         <h1>Все фотографии</h1>
-        {contents}
+        {this.renderContents()}
       </div>
     );
+  }
+
+  // NOTE: Логика отрисовки содержимого страницы такая:
+  // в зависимости от состояния либо показывается надпись «Загрузка...»,
+  // либо выводится сообщение об ошибки,
+  // либо показывается информация о фотографиях.
+  renderContents() {
+    if (this.state.loading) {
+      return (
+        <p>
+          <em>Загрузка...</em>
+        </p>
+      );
+    }
+
+    if (this.state.errorMessage != null) {
+      return (
+        <p>
+          <em>{this.state.errorMessage}</em>
+        </p>
+      );
+    }
+
+    return this.renderPhotos(this.state.photos);
   }
 
   renderPhotos(photos) {
@@ -54,6 +68,7 @@ export class Photos extends Component {
           <li>
             <b>Файл:</b> <span>{photo.fileName}</span>
           </li>
+          <span>{photo.url}</span>
         </ul>
       </div>
     );
@@ -66,9 +81,13 @@ export class Photos extends Component {
     const userId = user && user.sub;
     const accessToken = await authService.getAccessToken();
 
-    // NOTE: Если нет access token, то загрузка прекращается, а фотографии не показываются.
+    // NOTE: Если нет access token, то загрузка прекращается и показывается сообщение об ошибке.
     if (!accessToken) {
-      this.setState({ photos: [], loading: false });
+      this.setState({
+        photos: [],
+        loading: false,
+        errorMessage: "Отказано в доступе",
+      });
       return;
     }
 
@@ -80,17 +99,20 @@ export class Photos extends Component {
       }
     );
 
-    // NOTE: Похоже токен больше не действует, поэтому надо попробовать его обновить.
-    // Компонент Photos вложен в компонент AuthorizeRoute, поэтому после успешного выполнения signIn
-    // произойдет обновление страницы, а значит попытка загрузить фотографии компонентом Photos повторится.
+    // NOTE: Похоже токен больше не действует,
+    // поэтому завершаем загрузку и выводим сообщение об ошибке
     if (response.status === 401) {
-      await authService.signIn();
+      this.setState({
+        photos: [],
+        loading: false,
+        errorMessage: "Отказано в доступе",
+      });
       return;
     }
 
     // NOTE: Получение результата в виде JSON
     const data = await response.json();
     // NOTE: Обновление состояния страницы: фотографии получены, загрузка закончена.
-    this.setState({ photos: data, loading: false });
+    this.setState({ photos: data, loading: false, errorMessage: null });
   }
 }
