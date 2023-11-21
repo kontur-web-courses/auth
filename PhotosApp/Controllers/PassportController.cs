f032611d215a1d07424bfb979c977c9c36729c15
tereshkovyv <tyv2016@yandex.ru>
2023-11-21 15:17:36 +0500

    step 9.4

--- PhotosApp/Controllers/PassportController.cs
+++ PhotosApp/Controllers/PassportController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Authentication;
+﻿using System;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,11 +9,25 @@ namespace PhotosApp.Controllers
     {
         // NOTE: Неаутентифицированный пользователь будет отправляться на вход в DefaultChallengeScheme,
         // а затем возвращаться сюда и отсюда перенаправляться на исходную страницу из returnUrl.
-        public IActionResult Login(string returnUrl)
+        public IActionResult Login(bool rememberMe, string returnUrl)
         {
             if (!User.Identity.IsAuthenticated)
-                return Challenge();
-            
+            {
+                // NOTE: с помощью properties можно задать некоторые параметры будущей сессии.
+                // Основные же параметры сессии будут созданы внешним провайдером при обработке Challenge.
+                var properties = rememberMe
+                    ? new AuthenticationProperties
+                    {
+                        // NOTE: Кука будет сохраняться при закрытии браузера
+                        IsPersistent = true,
+                        // NOTE: Кука будет действовать 7 суток
+                        ExpiresUtc = DateTime.UtcNow.AddDays(7),
+                    }
+                    : null;
+
+                return Challenge(properties);
+            }
+    
             return Redirect(Url.IsLocalUrl(returnUrl) ? returnUrl : "/");
         }
 
